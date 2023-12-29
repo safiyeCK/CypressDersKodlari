f4deff4a658eab1ecec4cfce35230b8186c264cd
Safıye Kara <132605708+safiyeCK@users.noreply.github.com>
2023-12-29 02:02:04 +0100

    Update get01.cy.js

--- cypress/e2e/day01/get01.cy.js
+++ cypress/e2e/day01/get01.cy.js
@@ -56,9 +56,3 @@ describe("Get Method  Testing",()=>{
 });
 
 
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/safiyeCK/CypressDersKodlari.git
-    git push -u origin main
